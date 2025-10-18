71b14300007b3a07a7de2bf3dbff8b0e20d52b44
freegpu <freegpu102@gmail.com>
2025-10-18 20:18:10 +0545

    Prepare backend for Vercel serverless deployt

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 // api/index.js
 const serverless = require("serverless-http");
-const app = require("../server"); // Import your existing Express app
+const app = require("../server");
 
-module.exports.handler = serverless(app);
+module.exports = serverless(app); 
